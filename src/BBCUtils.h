dc4ecd644be4effbbb279e4e2cefc53e40047e62
radamchin <adam@bigbrowncupboard.com>
2014-06-01 17:59:50 +1000

    New general class with bunch of namespace static utils: clipboard access and time strings

--- src/BBCUtils.h
+++ src/BBCUtils.h
@@ -2,17 +2,84 @@
 /*
  
  BBCUtils
- BigBrownCupboard
+ 
+ General utilities, living here until somewhere more organized shows u
  
  Adam Hinshaw 2013
  
  
- 
  */
 
-#include "ErpEase.h"
-#include "Config.h"
+//#include "ErpEase.h"
+//#include "Config.h"
 
-#include "RandomBezierAnimator.h"
-#include "BezierAnimator.h"
-#include "FrameTweener.h"
+//#include "RandomBezierAnimator.h"
+//#include "BezierAnimator.h"
+//#include "FrameTweener.h"
+
+
+
+// For Clipboard access
+#if (_MSC_VER)
+#include <GLFW/glfw3.h>
+#else
+#include "GLFW/glfw3.h"
+#endif
+
+
+namespace bbc {
+    namespace utils {
+
+        static const string getUpTimeStr(bool show_secs = true) {
+            // How long uas this app been running? Return formated string of such.
+            
+            unsigned long total_up_secs = ofGetElapsedTimeMillis() / 1000;
+            
+            int dys = (int) floor(total_up_secs / 86400); // 86400 == 60 * 60 * 24, seconds in a day.
+            int hrs = (int) floor((total_up_secs % 86400) / 3600); // 3600 == 60 * 60. secopnds in an hour
+            int mins = (int) floor(((total_up_secs % 86400) % 3600) / 60);
+            int secs = (total_up_secs % 86400) % 3600 % 60;
+            
+            stringstream out;
+            out << "up:" << dys << "d," << hrs << "h," << mins << "m," << secs << "s";
+            return out.str();
+        }
+
+        static void setClipboard(string clippy) {
+            // Found in ofxTextInputField
+            // if win32 code not working, could just bail out.
+            
+            #if defined(TARGET_OSX)
+            glfwSetClipboardString( (GLFWwindow*) ofGetWindowPtr()->getCocoaWindow(), clippy.c_str());
+            #endif
+            
+            #if defined(TARGET_WIN32)
+            glfwSetClipboardString( (GLFWwindow*) ofGetWindowPtr()->getWin32Window(), clippy.c_str())
+            #endif
+        }
+        
+        static const string getClipboard(){
+            
+            #if defined(TARGET_OSX)
+            const char *clip = glfwGetClipboardString((GLFWwindow*) ofGetWindowPtr()->getCocoaWindow());
+            #endif
+            
+            #if defined(TARGET_WIN32)
+            const char *clip = glfwGetClipboardString((GLFWwindow*) ofGetWindowPtr()->getWin32Window());
+            #endif
+            
+            return (clip==NULL) ? "" : string(clip);
+        }
+        
+        static const string getUnixTimeStamp(bool show_ms = false) {
+            // get now in format  YYYYMMDDHHMMSSmmm
+            ostringstream format;
+            format << "%Y%m%d%H%M%S" << (show_ms ? "%i" : "");
+            
+            return ofGetTimestampString(format.str());
+        }
+
+        
+
+    }
+}
