948e67d044de3a2ac4d086cd86408a5781cc53fd
radamchin <adam@bigbrowncupboard.com>
2015-08-27 17:26:05 +1000

    Added bunch of date misc utils

--- src/BBCUtils.h
+++ src/BBCUtils.h
@@ -21,6 +21,12 @@
 
 #include "ofMain.h"
 
+#include "Poco/Timestamp.h"
+#include "Poco/DateTime.h"
+#include "Poco/DateTimeFormatter.h"
+#include "Poco/DateTimeParser.h"
+#include "Poco/DateTime.h"
+
 // For Clipboard access
 #if (_MSC_VER)
 #include <GLFW/glfw3.h>
@@ -34,6 +40,11 @@
 		return floor(f + .5);
 	}
 #endif
+
+
+#define bbcSQLDateStampFormat "%Y-%m-%d %H:%M:%S"    // "YYYY-MM-DD HH:MM:SS" : Format SQLite needs to save dates (or could just use  DateTimeFormat::SORTABLE_FORMAT for this..)
+#define bbcSQLDayFormat "%Y-%m-%d"                   // "YYYY-MM-DD"
+#define bbcSQLTimeFormat "%H:%M:%S"                  // "HH:MM:SS"
 
 
 namespace bbc {
@@ -60,6 +71,64 @@ namespace bbc {
             int seconds = round(totalSeconds - (hours * 3600) - (minutes * 60));
             
             return ofToString(hours, 2, '0') + ":" + ofToString(minutes, 2, '0') + ":" + ofToString(seconds, 2, '0');
+        }
+        
+        static string getNowSQLDateStamp() {
+            // "2015-02-23 15:39:22"
+            Poco::LocalDateTime now;
+            return Poco::DateTimeFormatter::format(now, bbcSQLDateStampFormat);
+        }
+        
+        static string getSQLDateStamp(Poco::LocalDateTime & t) {
+            return Poco::DateTimeFormatter::format(t, bbcSQLDateStampFormat);
+        }
+        
+        static string getNowSQLDay() {
+            // "2015-02-23"
+            Poco::LocalDateTime now;
+            return Poco::DateTimeFormatter::format(now, bbcSQLDayFormat);
+        }
+        
+        
+        
+        static string getNowTime() {
+            // just time "15:39:22"
+            Poco::LocalDateTime now;
+            return Poco::DateTimeFormatter::format(now, bbcSQLTimeFormat);
+        }
+        
+        static unsigned int getNowSeconds() {
+            return ofGetUnixTime();
+        }
+        
+        static bool isToday(Poco::LocalDateTime & date) {
+            Poco::LocalDateTime today;
+            
+            if(date.day() == today.day() && date.month() == today.month() && date.year() == today.year()) {
+                return true;
+            }
+            
+            return false;
+        }
+        
+        static bool isYesterday(Poco::LocalDateTime & date) {
+            Poco::LocalDateTime today;
+            
+            if(date.day() == today.day() && date.month() == today.month() && date.year() == today.year()) {
+                return false; // it's today.
+            }
+            
+            Poco::Timespan difference = today - date;
+            
+            int h = difference.totalHours();
+            int m = difference.totalMinutes();
+            int s = difference.totalSeconds();
+            
+            if(difference.totalHours() <= 24) {
+                return true;
+            }
+            
+            return false;
         }
         
        /* static string secondsToMS(int totalSeconds) {
@@ -171,7 +240,76 @@ namespace bbc {
             }
             
         }
+        
+        ///////////////////////////////////////////////////////////////////////////////////////
+        
+        static float getNextMultiple(int n, int multiple) {
+            // return next number, from #n that is a multiple of #multiple, but not if #n already multiple
+            //
+            // see: http://stackoverflow.com/questions/2403631/how-do-i-find-the-next-multiple-of-10-of-any-integer
+            
+            if (n % multiple){
+                n = n + (multiple - n % multiple);
+            }
+            
+            return n;
+        }
 
+        //---------------------------------------------------------------------------
+        
+        // These could have alts. that operate on the string passes instead
+        // i.e, void setToUpperCase void setToLowerCase
+        
+        static string toUpperCase(const string &  str) {
+            string strUpper = "";
+            
+            for( int i=0; i<str.length(); i++ ) {
+                strUpper += toupper( str[ i ] );
+            }
+            
+            return strUpper;
+        }
+        
+        static string toLowerCase(const string & str) {
+            string strLower = "";
+            
+            for( int i=0; i<str.length(); i++ ){
+                strLower += tolower( str[ i ] );
+            }
+            
+            return strLower;
+        }
+        
+        //---------------------------------------------------------------------------
+        
+        static string vecToStr(const ofVec3f & v) {
+            ostringstream out;
+            out << v.x << "," << v.y << "," << v.z;
+            return out.str();
+        }
+
+        static void printGLStats() {
+            
+            // Dump opengl info
+            ofLogNotice("\tGL") << "Vendor:   " << (char*)glGetString(GL_VENDOR);
+            ofLogNotice("\tGL") << "Renderer: " << (char*)glGetString(GL_RENDERER);
+            ofLogNotice("\tGL") << "Version:  " << (char*)glGetString(GL_VERSION);
+            ofLogNotice("\tGL") << "GLSL:     " << (char*)glGetString(GL_SHADING_LANGUAGE_VERSION);
+            //ofLogNotice("\tGL") << "Extensions:" << (char*)glGetString(GL_EXTENSIONS);
+            
+            int GlMaxTextureSize;
+            glGetIntegerv(GL_MAX_TEXTURE_SIZE, &GlMaxTextureSize);
+            ofLogNotice("\tGL") << "MaxTexSize:" << GlMaxTextureSize; // px?
+            
+            ofLogNotice("ofFbo") << "checkGLSupport:" << ofFbo::checkGLSupport() << ", maxColorAttachments=" << ofFbo::maxColorAttachments() << ", maxDrawBuffers=" << ofFbo::maxDrawBuffers() <<
+            ", maxSamples=" << ofFbo::maxSamples();
+            
+            if(ofIsGLProgrammableRenderer()) {
+                ofLogNotice("\tofGL Using Programmable Renderer");
+            }
+            
+        }
 
     }
+    
 }
